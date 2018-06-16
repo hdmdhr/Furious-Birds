919b9cdff7224c0cea8de9e5a9499134b4aca64a
DongMing <hdmdhr@gmail.com>
2018-06-16 00:06:05 -0600

    Add pinch gesture, pinch can only zoom in and out to maxScale; trace 1st pinch finger location and move camera there. Using extension of CGPoint to customize some operations. Need to add Bird class now.

---
+++ Birds/Configuration.swift
@@ -0,0 +1,24 @@
+//
+//  Configuration.swift
+//  Birds
+//
+//  Created by 胡洞明 on 2018/6/15.
+//  Copyright © 2018年 胡洞明. All rights reserved.
+//
+
+import CoreGraphics
+
+extension CGPoint {
+    
+    static public func + (left: CGPoint, right: CGPoint) -> CGPoint {
+        return CGPoint(x: left.x + right.x, y: left.y + right.y)
+    }
+    
+    static public func - (left: CGPoint, right: CGPoint) -> CGPoint {
+        return CGPoint(x: left.x - right.x, y: left.y - right.y)
+    }
+    
+    static public func * (left: CGPoint, right: CGFloat) -> CGPoint {
+        return CGPoint(x: left.x * right, y: left.y * right)
+    }
+}
--- Birds/GameScene.swift
+++ Birds/GameScene.swift
@@ -14,19 +14,23 @@ class GameScene: SKScene {
     var mapNode = SKTileMapNode()
     let gameCamera = GameCamera()
     var panRecognizer = UIPanGestureRecognizer()
+    var pinchRecognizer = UIPinchGestureRecognizer()
+    var maxScale : CGFloat = 0
     
     override func didMove(to view: SKView) {
         setupLevel()
         setupGestureRecognizer()
     }
     
-
+    
     
     // MARK: - Setup level(获得指向TileMapNode的var,用于setup camera constraint)
     
     func setupLevel(){
         if let mapNode = childNode(withName: "Tile Map Node") as? SKTileMapNode {
             self.mapNode = mapNode
+            maxScale = mapNode.mapSize.width / frame.width
+            print(maxScale)
         }
         setupCamera()
     }
@@ -38,6 +42,8 @@ class GameScene: SKScene {
         addChild(gameCamera)
         
         camera = gameCamera
+        gameCamera.xScale = maxScale  // 开始时将相机缩放至最小，显示整个场景
+        gameCamera.yScale = maxScale
         gameCamera.setConstraints(with: self, frame: mapNode.frame)
     }
     
@@ -47,8 +53,10 @@ class GameScene: SKScene {
         guard let gameSceneView = view else { fatalError("no view in the GameScene") }
         
         panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(pan))
-        
         gameSceneView.addGestureRecognizer(panRecognizer)
+        
+        pinchRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(pinch))
+        gameSceneView.addGestureRecognizer(pinchRecognizer)
     }
     
 }
@@ -57,8 +65,32 @@ class GameScene: SKScene {
 
 extension GameScene {
     @objc func pan(sender: UIPanGestureRecognizer) {
-        let translation = sender.translation(in: view)
+        let translation = sender.translation(in: view) * gameCamera.yScale  // configure to allow CGPoint * CGFloat
+        //        print("translation: \(translation)")  手指向左下↙移动时，translation值为(-, +)
         gameCamera.position = CGPoint(x: gameCamera.position.x - translation.x, y: gameCamera.position.y + translation.y)
         sender.setTranslation(.zero, in: view)
+    }
+    
+    @objc func pinch(sender: UIPinchGestureRecognizer) {
+        if sender.numberOfTouches == 2 {
+            // move the camera, so it zooms from where you pinch
+            let locationInView = sender.location(ofTouch: 1, in: view)
+            let locationB4 = convertPoint(fromView: locationInView)
+            
+            if sender.state == .changed {
+                let newScale = gameCamera.xScale / sender.scale  // 相机的xScale和sender.scale相反，缩小数值拉近镜头
+                if newScale <= maxScale && newScale >= 0.75 {  // 设置相机缩放的上下限
+                    gameCamera.setScale(newScale)
+                }
+                
+                let locationAfter = convertPoint(fromView: locationInView)
+                let locationDelta = locationB4 - locationAfter  // configure to allow +- CGPoint
+                let newPostion = gameCamera.position + locationDelta
+                gameCamera.position = newPostion
+                
+                sender.scale = 1.0
+                gameCamera.setConstraints(with: self, frame: mapNode.frame)
+            }
+        }
     }
 }
